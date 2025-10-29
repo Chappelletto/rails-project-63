ed17acffa4632e04fa5b9b022642ec5579b6d7f9
Peter <sirchappelletto@rambler.ru>
2025-10-29 16:29:30 +0400

    2: добавил метод генерирующий HTML для тегов

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -3,6 +3,24 @@
 require_relative "hexlet_code/version"
 
 module HexletCode
+  class Tag
+    def self.build(tag_name, **attributes, &block)
+      # реализация
+      if block_given?
+        "<#{tag_name}#{render_attributes(attributes)}>" + yield + "</#{tag_name}>"
+      else
+        "<#{tag_name}#{render_attributes(attributes)}/>"
+      end
+    end
+
+    def self.render_attributes(attributes)
+      return "" if attributes.none?
+
+      rendered = attributes.map { |key, value| "#{key}=\"#{value}\"" }.join(" ")
+      " " + rendered
+    end
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
