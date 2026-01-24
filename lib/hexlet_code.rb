f7f694de9ddebb1e59d47df10e9affeda48301fa
Peter <sirchappelletto@rambler.ru>
2026-01-24 19:10:16 +0400

    add label and submit

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,48 +1,159 @@
-# frozen_string_literal: true
+class Tag
+  attr_accessor :name, :attributes, :children
 
-require_relative "hexlet_code/version"
+  def initialize(name, **attributes)
+    @name = name
+    @attributes = attributes
+    @children = []
+  end
 
-module HexletCode
-  class Tag
-    def self.build(tag_name, **attributes, &block)
-      # реализация
-      if block_given?
-        "<#{tag_name}#{render_attributes(attributes)}>" + yield + "</#{tag_name}>"
-      else
-        "<#{tag_name}#{render_attributes(attributes)}></#{tag_name}>"
-      end
-    end
+  def add_children(children)
+    @children << children
+  end
+  alias_method :<<, :add_children
 
-    def self.render_attributes(attributes)
-      return "" if attributes.none?
-
-      rendered = attributes.map { |key, value| "#{key}=\"#{value}\"" }.join(" ")
-      " " + rendered
+  def to_html
+    if @children.any?
+      "<#{@name}#{render_attributes}>" + render_children + "</#{@name}>"
+    else
+      "<#{@name}#{render_attributes}/>"
     end
   end
 
-  def self.form_for(user, **attributes)
-    default_attributes = {action: "#", method: "post"}
-    if attributes[:url]
-      attributes[:action] = attributes[:url]
-      attributes.delete(:url)
-    end
+  def render_attributes
+    return "" if @attributes.none?
 
-    # default_attributes.merge(attributes)
-
-    Tag.build("form", **default_attributes.merge(attributes))
+    rendered = @attributes.map { |key, value| "#{key}=\"#{value}\"" }.join(" ")
+    " " + rendered
   end
 
-  class Error < StandardError; end
-  # Your code goes here...
+  def render_children
+    @children.map { |c| c.to_html }.join
+  end
 end
 
-# puts user.name
-#=> rob
-# pp HexletCode::Tag.build("br")
-# pp HexletCode.form_for("")
+class FormTag < Tag
+  def initialize(object = nil, **attributes)
+    @object = object
+    default_attributes = {action: "#", method: "POST"}
+    super(
+      "form",
+      **default_attributes.merge(attributes)
+    )
+  end
 
-# <br/>
-# <p>text</p>
-# <div class="button" data-link="https://ya.ru"/>
-# <div class="button" data-link="https://ya.ru"> text </div>
+  def input(name, **attributes)
+    as = attributes.delete(:as)
+    value = @object&.public_send(name)
+
+    id = attributes[:id] || name
+    label_text = attributes.delete(:label) || name.to_s.capitalize
+    label = LabelTag.new(id, label_text)
+
+    attrs = {name: name, value: value}.merge(attributes)
+
+    input = if as == :text
+      TextareaTag.new(**attrs)
+    else
+      InputTag.new(**attrs)
+    end
+
+    # self << input
+    # self.add_children(input)
+    self << label
+    add_children(input)
+  end
+
+  def submit(value = "Save", **attributes)
+    attrs = {
+      type: "submit",
+      value: value
+    }.merge(attributes)
+
+    self << InputTag.new(**attrs)
+  end
+end
+
+class LabelTag < Tag
+  def initialize(for_id, text)
+    super("label", for: for_id)
+    @text = text
+  end
+
+  def to_html
+    "<#{@name}#{render_attributes}>#{@text}</#{@name}>"
+  end
+end
+
+class InputTag < Tag
+  def initialize(**attributes)
+    default_attributes = {type: "text", value: ""}
+    super(
+      "input",
+      **default_attributes.merge(attributes)
+    )
+  end
+end
+
+class TextareaTag < Tag
+  attr_accessor :value
+
+  def initialize(**attributes)
+    default_attributes = {cols: "20", rows: "40"}
+    @value = attributes.delete(:value) || ""
+
+    super(
+      "textarea",
+      **default_attributes.merge(attributes)
+    )
+  end
+
+  def to_html
+    "<#{@name}#{render_attributes}>" + @value + "</#{@name}>"
+  end
+end
+
+class HexletCode
+  def self.form_for(object)
+    form = FormTag.new(object)
+    yield form
+
+    form.to_html
+  end
+end
+
+form = FormTag.new
+name_input = InputTag.new(name: "name", value: "rob")
+# job_input = InputTag.new(name: "job", value: "hexlet", as: "text")
+job_input = TextareaTag.new(name: "job", value: "hexlet")
+
+form << name_input
+form << job_input
+# pp form
+# puts form.to_html
+# pp ""
+# pp ""
+# pp ""
+# pp "---"
+
+# HexletCode.form_for user do |form_builder| # form_builder - это наш FormBuilder
+#   # Проверяет есть ли значение внутри name
+#   form_builder.input :name
+#   # Проверяет есть ли значение внутри job
+#   form_builder.input :job, as: :text
+# end
+
+User = Struct.new(:name, :job, :gender, keyword_init: true)
+user = User.new name: "rob", job: "hexlet", gender: "m"
+
+form_1 = HexletCode.form_for user do |form|
+  form.input :name, class: "user-input"
+  form.input :job, as: :text, cols: "50", rows: "50"
+  form.submit
+  # form.input :age
+end
+puts form_1
+
+# Задачи
+# Реализуйте label для каждого инпута
+# Реализуйте метод submit()
