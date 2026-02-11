3e881104ff03f0ed523a3132a3f5c26cbbe7d11d
Peter <sirchappelletto@rambler.ru>
2026-02-12 00:23:10 +0400

    add minitest

---
+++ test/hexlet_code_test.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class HexletCodeTest < Minitest::Test
+  def test_empty_form
+    user = User.new name: "rob"
+    expected = read_fixture("empty_form")
+
+    # rubocop:disable Lint/EmptyBlock
+    actual = HexletCode.form_for user do |f|
+    end
+    # rubocop:enable Lint/EmptyBlock
+
+    assert { actual == expected }
+  end
+
+  def test_form_with_submit
+    user = User.new job: "hexlet"
+    expected = read_fixture("with_submit")
+
+    actual = HexletCode.form_for user, url: "#" do |f|
+      f.input :name, class: "user-input"
+      f.input :job, as: :text, rows: 50, cols: 50
+      f.submit "Wow"
+    end
+
+    assert_equal actual, expected
+  end
+
+  def test_with_undefined_model_method
+    user = User.new name: "rob", job: "hexlet", gender: "m"
+
+    assert_raises NoMethodError do
+      HexletCode.form_for user, url: "/users" do |f|
+        f.input :name
+        f.input :job, as: :text
+        # Поля age у пользователя нет
+        f.input :age
+      end
+    end
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "simplecov"
+SimpleCov.start
+
+require "simplecov-cobertura"
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
+require "hexlet_code"
+
+require "minitest/autorun"
+require "minitest-power_assert"
+
+User = Struct.new(:name, :job, :gender, keyword_init: true)
+
+module Minitest
+  class Test
+    def read_fixture(name)
+      fixture_path = File.expand_path("#{__dir__}/fixtures/#{name}.html")
+
+      File.read(fixture_path).strip
+    end
+  end
+end
